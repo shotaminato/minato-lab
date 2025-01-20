4245c016f5de3d3a6616008e3d2280be4a363981
Shota <tyuntune@gmail.com>
2025-01-19 20:15:08 -0800

    [Chore] File organization.

--- post_processing.py
+++ post_processing.py
@@ -1,15 +1,22 @@
-import os, subprocess
+import os, subprocess, shutil
 from datetime import datetime
 
 # pages directory
-SRC_DIR = 'pages'
-TARGET_DIR = "modules/ROOT/"
+SRC_DIR = 'ROOT'
+TARGET_DIR = "modules"
 
 def main():
-    # Get the list of files in the pages directory recursively
-    for root, dirs, files in os.walk(SRC_DIR):
+    # Copy dir to the target dir
+    os.makedirs(f'{TARGET_DIR}/{SRC_DIR}', exist_ok=True)
+    
+    shutil.copy2   (f"{SRC_DIR}/nav.adoc", f"{TARGET_DIR}/{SRC_DIR}/nav.adoc")
+    shutil.copytree(f"{SRC_DIR}/images"  , f"{TARGET_DIR}/{SRC_DIR}/images"  , dirs_exist_ok=True)
+    shutil.copytree(f"{SRC_DIR}/partials", f"{TARGET_DIR}/{SRC_DIR}/partials", dirs_exist_ok=True)
+
+    # Walk through the pages directory
+    for cur_dir, dirs, files in os.walk(f"{SRC_DIR}/pages"):
         for file in files:
-            file_path = os.path.join(root, file)
+            file_path = os.path.join(cur_dir, file)
             
             # Get created and modified date of the file (yyyy-mm-dd hh:mm:ss)
             modified_timestamp = subprocess.check_output(f'git log --format=%ci -- {file_path} | head -n 1', shell=True).decode().strip()
@@ -47,12 +54,12 @@ def main():
                 if not is_cdate_set: processed_content.insert(0, f':postdate: {created_timestamp}\n')
                 if not is_mdate_set: processed_content.insert(1, f':revdate: {modified_timestamp}\n')
 
-                os.makedirs(TARGET_DIR + root, exist_ok=True)
+                os.makedirs(f'{TARGET_DIR}/{cur_dir}', exist_ok=True)
 
                 processed_content.insert(0, f"include::partial$template.adoc[]\n\n")
 
                 # Open the file in write mode
-                with open(f"{TARGET_DIR}/{root}/{file}", 'w') as wf:
+                with open(f"{TARGET_DIR}/{cur_dir}/{file}", 'w') as wf:
                     # Write the processed content back to the file
                     wf.writelines(processed_content)
 
